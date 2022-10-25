0bf2412f9d7cf9736f967e38b798184fa8138370
csouthern54 <csouthern@cherrycreekschools.org>
2022-10-25 10:39:10 -0600

    yes

---
+++ New Unity Project/Assets/Sunnyland/artwork/Scripts/The End is Near.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheEndisNear : MonoBehaviour
+{
+    int first = 10;
+    Rigidbody2D Player = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log(first);
+        Debug.Log(Player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
